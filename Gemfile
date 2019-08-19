0fffd10ed104d1e2e84e350ac8fbb02404808127
notmarkmiranda <notmarkmiranda@gmail.com>
2019-08-19 06:42:15 -0600

    add dashboard and login feature spec

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ group :development do
 end
 
 group :test do
+  gem "capybara"
   gem "database_cleaner"
   gem "shoulda-matchers"
 end
---
+++ spec/features/log_in/visitor_can_log_in_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe "Visitor can log in", type: :feature do
+  let(:user) { create(:user) }
+
+  before do
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+  end
+
+  describe "with correct email / password" do
+    it "allows the user to log in" do
+      fill_in 'Password', with: 'password'
+      click_button "Let's Go!"
+
+      expect(page).to have_content "dashboard"
+    end
+  end
+
+  describe "with incorrect email / password" do
+    it "does not allow the user to log in" do
+      fill_in "Password", with: "passsssssword"
+      click_button "Let's Go!"
+
+      expect(page).to have_content "Email"
+      expect(page).not_to have_content "dashboard"
+    end
+  end
+end
